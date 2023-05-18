9ba97d262072224f7b20390c4cae9687a3162c33
root <root@ryzen2700x.ellipsoid.ca>
2023-05-17 19:39:34 -0600

    Initial commit

---
+++ lib/Controller/PageController.php
@@ -0,0 +1,28 @@
+<?php
+declare(strict_types=1);
+// SPDX-FileCopyrightText: Pavlo Karbovnyk <pkarbovn@gmail.com>
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+namespace OCA\LLMVirtualUser\Controller;
+
+use OCA\LLMVirtualUser\AppInfo\Application;
+use OCP\AppFramework\Controller;
+use OCP\AppFramework\Http\TemplateResponse;
+use OCP\IRequest;
+use OCP\Util;
+
+class PageController extends Controller {
+	public function __construct(IRequest $request) {
+		parent::__construct(Application::APP_ID, $request);
+	}
+
+	/**
+	 * @NoAdminRequired
+	 * @NoCSRFRequired
+	 */
+	public function index(): TemplateResponse {
+		Util::addScript(Application::APP_ID, 'llmvirtualuser-main');
+
+		return new TemplateResponse(Application::APP_ID, 'main');
+	}
+}
---
+++ tests/Unit/Controller/NoteApiControllerTest.php
@@ -0,0 +1,15 @@
+<?php
+declare(strict_types=1);
+// SPDX-FileCopyrightText: Pavlo Karbovnyk <pkarbovn@gmail.com>
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+namespace OCA\LLMVirtualUser\Tests\Unit\Controller;
+
+use OCA\LLMVirtualUser\Controller\NoteApiController;
+
+class NoteApiControllerTest extends NoteControllerTest {
+	public function setUp(): void {
+		parent::setUp();
+		$this->controller = new NoteApiController($this->request, $this->service, $this->userId);
+	}
+}
